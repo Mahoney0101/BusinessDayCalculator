9ac454883b5a6f7554a4644a7ee9a9d5bbe44493
mahoney0101 <mahoneyjames67@gmail.com>
2021-08-29 10:04:19 +0100

    Initial commit

---
+++ businessdays/BusinessDays.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace businessdays
+{
+    public static class BusinessDays
+    {
+        public static DateTime AddBusinessDays(this DateTime source, int businessDays)
+        {
+            var dayOfWeek = businessDays <= 0
+                                ? ((int)source.DayOfWeek - 12) % 7
+                                : ((int)source.DayOfWeek + 6) % 7;
+
+            switch (dayOfWeek)
+            {
+                case 6:
+                    businessDays--;
+                    break;
+                case -6:
+                    businessDays++;
+                    break;
+            }
+
+            return source.AddDays(businessDays + ((businessDays + dayOfWeek) / 5) * 2);
+        }
+        public static DateTime SubtractBusinessDays(this DateTime source, int businessDays)
+        {
+            return AddBusinessDays(source, -businessDays);
+        }
+    }
+}
